04ad5c7213039a59e6048da6c1df3241077824a5
Robert Schili <rschili@users.noreply.github.com>
2021-07-03 18:27:16 +0200

    Update SchemaFile.cs

--- SchemaFile.cs
+++ SchemaFile.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace SchemaFragmentExtractor
@@ -22,6 +24,12 @@ namespace SchemaFragmentExtractor
         public XDocument? Document { get; private set; }
         public string Label { get; set; }
 
+        public string? SchemaName { get; set; }
+
+        public string? Version { get; set; }
+
+        public List<ECClass> Classes { get; } = new List<ECClass>();
+
         internal async Task LoadAsync()
         {
             try
@@ -32,18 +40,57 @@ namespace SchemaFragmentExtractor
                     Document = doc;
                     Label = $"{FullPath} - Loading Contents...";
                     PerformPropertyChanged(nameof(Label));
+                    await Task.Run(() => LoadContents());
+                    int? lineNumber = (Document.LastNode as IXmlLineInfo)?.LineNumber;
+                    Label = $"{SchemaName} {Version} ({Classes.Count} classes, {lineNumber} Lines)";
+                    PerformPropertyChanged(nameof(Label));
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Label = $"'{FullPath}' failed to load: {e.Message}";
                 PerformPropertyChanged(nameof(Label));
                 Broken = true;
             }
+        }
 
-            await Task.Delay(2000);
-            Label = $"{FullPath} - Ready.";
-            PerformPropertyChanged(nameof(Label));
+        private Regex _classCheckRegex = new Regex(@"^EC(\w*)Class$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        internal bool IsClassElement(XElement element)
+        {
+            if (element == null) return false;
+            return _classCheckRegex.IsMatch(element.Name.LocalName);
+        }
+
+        internal void LoadContents()
+        {
+            if (Document == null) throw new InvalidOperationException("No Document available.");
+            var root = Document.Root;
+            SchemaName = root?.Attribute("schemaName")?.Value;
+            Version = root?.Attribute("version")?.Value;
+            if (SchemaName == null || Version == null || root == null)
+                throw new InvalidDataException("Format unknown.");
+
+            foreach (var child in root.Elements())
+            {
+                if (!IsClassElement(child))
+                    continue;
+
+                var typeName = child.Attribute("typeName")?.Value;
+                if (typeName == null) continue; // Should never happen?
+                Classes.Add(new ECClass(typeName, child));
+            }
+        }
+    }
+
+    public class ECClass
+    {
+        public string TypeName { get; init; }
+        public XElement Element { get; init; }
+
+        public ECClass(string typeName, XElement child)
+        {
+            TypeName = typeName;
+            Element = child;
         }
     }
 }
